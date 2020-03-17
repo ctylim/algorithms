34c8dc7887a3d509ddc888e1f056c29964f79399
ctylim <lim.0.ctyl@gmail.com>
2020-03-18 08:52:43 +0900

    many_aplusb

---
+++ src/many_aplusb.rs
@@ -0,0 +1,46 @@
+fn main() {
+    let (r, w) = (std::io::stdin(), std::io::stdout());
+    let mut io = IO::new(r.lock(), w.lock());
+    let n: usize = io.read();
+    for _ in 0..n {
+        let (a, b): (i64, i64) = (io.read(), io.read());
+        io.write(format!("{}\n", a + b));
+    }
+}
+
+pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
+
+impl<R: std::io::Read, W: std::io::Write> IO<R, W> {
+    pub fn new(r: R, w: W) -> IO<R, W> {
+        IO(r, std::io::BufWriter::new(w))
+    }
+    pub fn write(&mut self, s: String) {
+        use std::io::Write;
+        self.1.write(s.as_bytes()).unwrap();
+    }
+    pub fn writeln(&mut self, s: String) {
+        use std::io::Write;
+        self.1.write(format!("{}\n", s).as_bytes()).unwrap();
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .0
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r' || b == b'\t')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r' && b != b'\t')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
